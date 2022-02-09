0e4a44beec74b3a487617ea6313c9997c146ab6b
HilmiArizal <hilmi.arizal36@gmail.com>
2022-02-09 16:18:54 +0700

    change-url

--- src/Services/API_Local.js
+++ src/Services/API_Local.js
@@ -1,2 +1,2 @@
-// export const API_URL_LOCAL = 'https://sarenone-api.herokuapp.com/';
-export const API_URL_LOCAL = 'http://localhost:8000/';
+export const API_URL_LOCAL = 'https://sarenone-api.herokuapp.com/';
+// export const API_URL_LOCAL = 'http://localhost:8000/';
